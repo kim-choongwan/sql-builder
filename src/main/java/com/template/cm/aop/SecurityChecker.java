cdc655360336916af54d7aca70479cffcd62edf0
kim.choong.wan <kim.choong.wan@gmail.com>
2020-11-19 15:22:05 +0900

    aop sample

---
+++ src/main/java/com/template/cm/aop/SecurityChecker.java
@@ -0,0 +1,41 @@
+package com.template.cm.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SecurityChecker {
+
+	private static final Logger logger = LoggerFactory.getLogger(SecurityChecker.class);
+
+	@Pointcut("@annotation(SecurityCheck)")
+	public void checkMethodSecurity() {
+	}
+
+	@Around("checkMethodSecurity()")
+	public Object checkSecurity(ProceedingJoinPoint joinPoint) throws Throwable {
+		logger.debug("Checking method security...");
+		// TODO Implement security check logics here
+		Object result = joinPoint.proceed();
+		return result;
+	}
+
+//	@Pointcut("execution(* app.messages..*.*(..))")
+//	@Pointcut("execution(* app.messages..*Service.*(..))")
+//	public void everyMessageMethod() {
+//		logger.debug("## everyMessageMethod");
+//	}
+//	
+//	@Around("everyMessageMethod()")
+//	public Object checkSecurity( ProceedingJoinPoint joinPoint ) throws Throwable {
+//		logger.debug("## checkSecurity");
+//		return joinPoint.proceed();
+//	}
+	
+}
--- src/main/java/com/template/sample/MessageService.java
+++ src/main/java/com/template/sample/MessageService.java
@@ -1,15 +1,9 @@
 package com.template.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.template.cm.aop.SecurityCheck;
 
 @Service
 public class MessageService {
@@ -17,6 +11,7 @@ public class MessageService {
 	@Autowired
 	MessageRepository repository;
 
+	@SecurityCheck
 	public Message save(String text) {
 		return repository.saveMessage(new Message(text));
 	}
