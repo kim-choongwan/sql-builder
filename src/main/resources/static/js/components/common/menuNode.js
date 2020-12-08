6284363f64c36d82c9d83a6292e7343a8a8f8844
ywyi1992 <ywyi1107@gmail.com>
2020-12-08 19:16:59 +0900

    menu component update

--- src/main/resources/static/js/components/common/menuNode.js
+++ src/main/resources/static/js/components/common/menuNode.js
@@ -1,22 +1,43 @@
+import menuHeader from "./menuHeader.js";
 export default{
 	name : `menuNode`,
 	template : `<ul>
-					<template v-if="typeof(element) === 'string'">
-						<li>
-							{{ element }}
-						</li>
-					</template>
-					<template v-else>
-						<li v-for="(value, key) in element" :key="key" :id="key">
-							<p :class="Object.keys(value).length > 0 ? 'item-up' : 'item-empty'"><span class="menu-icon"></span>{{ key }}</p>
-						</li>
-					</template>
-				</ul>	
+					<li v-if="typeof(element) === 'string'">
+						<menu-header :title="element"/>
+					</li>
+					<li v-else-if="!Array.isArray(element)" v-for="(value, key) in element" :key="key" :id="key">
+						<menu-header :title="key" :size="Object.keys(value).length"/>
+						<menu-node v-if="showFlag" v-for="(element, title) in value" :key="title" :element="element" :iden="key + '-' + title"/>
+					</li>
+					<li v-else :id="iden">
+						<menu-header :title="kategorie" :size="element.length" :iden="iden"/>
+						<menu-node v-if="showFlag" v-for="(value, index) in element" :element="value" :key="index"/>
+					</li>
+				</ul>
 	`,
 	props : {
 		element : {
 			type : [Array,Object,String],
 			required : true
+		},
+		iden : {
+			type : String
+		},
+		title : {
+			type : String
 		}
+	},
+	data : function(){
+		return {
+			showFlag : false
+		}
+	},
+	computed : {
+		kategorie(){
+			return this.iden.split("-")[1];
+		}
+	},
+	components : {
+		menuHeader
 	}
 }
